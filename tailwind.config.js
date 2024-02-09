ef4a2b4bf8c1e8dc35eb2db7ea08731f2210549e
Abenezer <abenezerkifle000@gmail.com>
2024-02-09 06:43:57 -0800

    Add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        primaryBgColor: "#0067FF",
+        primaryColor: "#0067FF",
         yellowColor: "#FEB60D",
         purpleColor: "#9771FF",
         irisBlueColor: "#01B5C5",
